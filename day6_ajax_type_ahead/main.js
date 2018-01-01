2e63f272193865df519b39c54cb4581d4a09347b
ej <ej@AppledeMacBook-Pro-3.local>
2018-01-01 22:19:54 +0800

    day6 finished

---
+++ day6_ajax_type_ahead/main.js
@@ -0,0 +1,37 @@
+const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+const cities =[];
+fetch(endpoint)
+	.then(blob => blob.json())
+	.then(data => cities.push(...data)) //ES6 Spread Operator
+
+function findMatches(wordToMatch, cities) {
+	const regex = new RegExp(wordToMatch, 'gi');  //正規表達式g:找出所有否則只找出一個，i:不分大小寫
+	return cities.filter(place => place.city.match(regex) || place.state.match(regex))
+}
+
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');  //還很不熟悉正規表達式，這個function先暫時直接複製Wes老師的code來用><
+}
+
+function displayMatches() {
+	const matches = findMatches(this.value, cities);
+	// console.log(matches);
+	const html = matches.map(match => {
+		const regex = new RegExp(this.value, 'gi');
+		const cityName = match.city.replace(regex, `<span class="match_worlds">${this.value}</span>`); //將input的字串replace成有style的input字串
+		const stateName = match.state.replace(regex, `<span class="match_worlds">${this.value}</span>`);
+		return `
+		<li>
+			<span>${cityName} ${stateName}</span>
+			<span>${numberWithCommas(match.population)}</span>  
+		</li>`}).join('');  //先用map()將資料一個個轉換成html格式。但是因為用map()出來的格式是array，所以用join()將資料轉成string才不會有‘,’
+	outputs.innerHTML = html;
+}
+
+console.log(cities);
+
+const searchInput = document.querySelector('.search');
+const outputs = document.querySelector('.suggestions');
+
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
